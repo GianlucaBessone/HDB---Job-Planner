849c871030185b1def47fbe7c3858245961af84c
Gianluca Bessone <bessone.gianluca@gmail.com>
2026-06-16 11:45:03 -0300

    fix: enforce two decimal places for project cost currency formatting

--- components/projects/ProjectCostModal.tsx
+++ components/projects/ProjectCostModal.tsx
@@ -91,6 +91,8 @@ export default function ProjectCostModal({
         return new Intl.NumberFormat('es-AR', {
             style: 'currency',
             currency: 'ARS',
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
         }).format(amount);
     };
 
